578aba0aa5474b67befc55a4e4137aa41e44236a
spioneracorei8 <bestza147.aa@gmail.com>
2023-12-09 15:38:55 +0700

    feat: created user and chat message model successfully

---
+++ src/models/chatMessage.model.ts
@@ -0,0 +1,19 @@
+import { Schema } from "mongoose";
+
+interface IChatMessage extends Document {
+    userId: string,
+    name: string,
+    role: string,
+    message: string,
+    createdAt: Date,
+    updateAt: Date,
+}
+
+const chatMessageSchema = new Schema<IChatMessage>({
+    userId: { type: String, default: String, required: true },
+    name: { type: String, default: String },
+    role: { type: String, default: String, required: true },
+    message: { type: String, default: String, required: true },
+    createdAt: { type: Date, default: new Date() },
+    updateAt: { type: Date, default: new Date() },
+})
